9915cdf1d92e724cf000897ebbaa6eacecef24a0
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-08 09:21:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Generics =    enables types(classes and methods) to be parameters when defining
+        //              classes, interfaces and methods
+        //              a benefit is to eliminate the need to create multiple visions
+        //              of methods or classes fro various data types
+        //              Use 1 version for all reference data types
+
+
+    }
+}
